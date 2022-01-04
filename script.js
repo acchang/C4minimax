2cbd5dcbecf2039e523feb30976512ffffbd6fc4
Andrew Chang <acchang4@gmail.com>
2022-01-03 19:25:22 -0500

    gameboard in, playround()

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ var gameboard = [[1,2,3,4,5,6,7],
                  [15,16,17,18,19,20,21],
                  [22,23,24,25,26,27,28],
                  [29,30,31,32,33,34,35],
-                 [36,37,38,39,40,41,42]];
+                 [36,37,'X',39,40,41,42]];
 
-console.log("splice which number?")
+// need gameplay to switch rounds and then build a column
+// then scoring mechanism
+// then minimax
 
-// take in an input
-// splice it into the array
+function swapTurns() {
 
+}
 
+// 'X' is the token below, could be if space is not a number
+
+function playRound() {
+    const playerEnters = prompt('splice which column 1-7?');
+    const columnPick = parseInt(playerEnters);
+    let i;
+    for (i = 5; i > -1; i--) 
+    {if (Number.isInteger(gameboard[i][columnPick-1]))
+     {gameboard[i].splice((columnPick -1), 1, 'X')
+     console.log(columnPick + gameboard[i])
+     return}
+    };
+    };
+
+playRound()
