814ca28e7b120b5901d44f3212e6d12dad16274c
Andrew Chang <acchang4@gmail.com>
2022-01-07 11:31:00 -0500

    messing with canvas

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ var gameboard = [
                  [36,37,38,39,40,41,42]
                 ];
 // represent this graphically
+// draw gameboard
+// every time gameboard changes, html changes
+// use a nested loop to place the token
+// if playerOne, add class; if playerTwo, add other
+// is this right, is there any other way to do it? getContext never used
 
 var playerOne
 var playerTwo
@@ -18,19 +23,57 @@ var playerOneTurn = true
 var itsAOnePlayerGame = true
 var isThereAWinner = false
 
-function onePlayerSimpleOrAdvanced(){
-    simpleOrAdvanced = prompt('Simple(1) or Advanced(2)')
-    if (simpleOrAdvanced == 1) {
-        play1PGame()
-    }
-    else if (simpleOrAdvanced == 2) {
-        playAdvanced1PGame()
-    }
-    else {alert("No")
-        isThereAWinner = true
-        return
+//Draw Board
+var tileWidth = 125;
+var tileHeight = 125;
+
+var canvas = document.createElement("canvas");
+canvas.width = gameboard[0].length * tileWidth;
+canvas.height = gameboard.length * tileHeight;
+document.body.appendChild(canvas);
+var context = canvas.getContext("2d");
+
+function drawBoard() {
+for (var a = 0; a < gameboard.length; a++) {
+    for (var b = 0; b < gameboard[0].length; b++) {
+  
+      if (a % 2 == 0) {
+        if (b % 2 == 0) {
+          context.fillStyle = "#aaaaaa";
+        } else {
+          context.fillStyle = "#888888";
         }
+      } else {
+        if (b % 2 == 1) {
+          context.fillStyle = "#aaaaaa";
+        } else {
+          context.fillStyle = "#888888";
+        }
+      }
+      context.fillRect(b * tileWidth, a * tileHeight, tileWidth, tileHeight);
+      switch (gameboard[a][b]) {
+        case "W":
+          context.fillStyle = "#ffffff";
+          context.beginPath();
+          context.arc(b * tileWidth + tileWidth / 2, a * tileHeight + tileHeight / 2, 10, 0, Math.PI * 2, true);
+          context.closePath();
+          context.fill();
+          break;
+        case "B":
+          context.fillStyle = "#000000";
+          context.beginPath();
+          context.arc(b * tileWidth + tileWidth / 2, a * tileHeight + tileHeight / 2, 10, 0, Math.PI * 2, true);
+          context.closePath();
+          context.fill();
+          break;
+      }
+    }
+    }
 };
+
+
+
+// Begin Game
 
 function oneOrTwoPlayerGame(){
     typeOfGame = prompt('One or Two Player')
@@ -50,6 +93,30 @@ function oneOrTwoPlayerGame(){
         }
 };
 
+function onePlayerPickSides(){
+    playerOne = prompt('Your name')
+    playerTwo = 'Computer'
+};
+
+function twoPlayerPickSides() {
+    playerOne = prompt('Player One name')
+    playerTwo = prompt('Player Two name')
+};
+
+function onePlayerSimpleOrAdvanced(){
+    simpleOrAdvanced = prompt('Simple(1) or Advanced(2)')
+    if (simpleOrAdvanced == 1) {
+        play1PGame()
+    }
+    else if (simpleOrAdvanced == 2) {
+        playAdvanced1PGame()
+    }
+    else {alert("No")
+        isThereAWinner = true
+        return
+        }
+};
+
 function whosPlaying() {
     if(playerOneTurn) {
     return playerOne
@@ -58,27 +125,17 @@ function whosPlaying() {
     }
 };
 
-function onePlayerPickSides(){
-    playerOne = prompt('Who are you?')
-    playerTwo = 'Computer'
-};
-
-function twoPlayerPickSides() {
-    playerOne = prompt('Player One chooses')
-    playerTwo = prompt('Player Two chooses')
+function play1PGame() {
+    while (isThereAWinner == false) {
+        playerSelects1P()
+        placeToken()
+    }
 };
 
 function playAdvanced1PGame() {
     while (isThereAWinner == false) {
         playerSelects1P()
         // advancedPlayerSelects1P()
-        placeToken()
-    }
-};
-
-function play1PGame() {
-    while (isThereAWinner == false) {
-        playerSelects1P()
         placeToken()
     }
 };
@@ -138,7 +195,8 @@ function placeToken() {
 };
 
 // WIN CHECKERS
-// a for loop evaluates a section of the matrix, moving through it and seeing if the 3 ahead match.
+// a forloop evaluates a section of the matrix, moving through it and seeing if the 3 ahead match.
+// it stops before going out of bounds
 
 function findFour(w,x,y,z) {
     // Checks first cell against current player and all cells match that player
@@ -198,4 +256,5 @@ function horizontalCheck() {
     }
 };
 
+drawBoard()
 oneOrTwoPlayerGame()
