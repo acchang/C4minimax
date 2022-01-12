8a748d2fbdcb52dc241cc5eb9e0a1a7c20dd8444
Andrew Chang <acchang4@gmail.com>
2022-01-11 20:02:26 -0500

    working board

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ let gameboard = [
 
 let playerOne
 let playerTwo
-let columnPick
-let currentIndex
-let currentRow
+let indexPick
 let availableSpots
+let gameType
 let playerOneTurn = true
 let itsAOnePlayerGame = true
 let isThereAWinner = false
@@ -29,11 +28,11 @@ let selectorTable = document.createElement("table")
 selectorTable.setAttribute('class', 'selectorTable')
 selectorHolder.append(selectorTable)
 
-let selectorRow = document.createElement("tr") 
-selectorRow.setAttribute('class', 'selectorRow')
-selectorTable.append(selectorRow)
+function drawSelector() {  
+    let selectorRow = document.createElement("tr") 
+    selectorRow.setAttribute('class', 'selectorRow')
+    selectorTable.append(selectorRow)
 
-function drawSelectorCell() {  
     for (i=0; i<7; i++){
         let selectorCell = document.createElement("td") 
         selectorCell.setAttribute('class', 'selectorCell')
@@ -44,28 +43,40 @@ function drawSelectorCell() {
         selectorCell.append(innerSelectorCell)
         
         innerSelectorCell.addEventListener("mouseover", function(event) {
-            innerSelectorCell.classList.add('redBG')
+            if (playerOneTurn == true) {
+            innerSelectorCell.classList.add('yellowBG')}
+            else {innerSelectorCell.classList.add('redBG')
+            }
         })
 
         innerSelectorCell.addEventListener("mouseout", function(event) {
-            innerSelectorCell.classList.remove('redBG')
+            if (playerOneTurn == true) {
+            innerSelectorCell.classList.remove('yellowBG')}
+            else {innerSelectorCell.classList.remove('redBG')
+            }
         })
 
         innerSelectorCell.onclick = function(){
-            columnPick = this.id + 1
-            console.log(parseInt(this.id)+1)
+            indexPick = parseInt(this.id)
+            console.log(indexPick)
+            claimSpot()
+            swapTurns()
         }
 
         selectorRow.append(selectorCell)
-        }
+        }        
 };
 
-drawSelectorCell()
+drawSelector()
+
+
+
+
 
 // Draw Main Gameboard
 
 let mainTable = document.createElement("table");
-mainTable.id = 'mainTable'
+mainTable.setAttribute('class', 'mainTable')
 mainDiv.append(mainTable)
 
 function drawBoard() {
@@ -79,135 +90,68 @@ function drawBoard() {
                     row.append(outerCell)
                     let innerCell = document.createElement('div')
                     innerCell.setAttribute('class', 'innerCell')
-                    innerCell.setAttribute('class', gameboard[i][j])
+                    innerCell.classList.add(gameboard[i][j])
                     innerCell.setAttribute('innerHTML', gameboard[i][j])
                     outerCell.append(innerCell)
+                }   
             }
-        }
 };
 
 drawBoard()
 
-//Draw gameTypeDropDownArea
-// to avoid the show function being written for a button that won't exist when the function is parsed I used radio buttons
-
-let gameInfoAreaDiv = document.createElement("div") 
-gameInfoAreaDiv.id = 'gameInfoArea'
-mainDiv.append(gameInfoAreaDiv);
-
-let gameTypeDiv = document.createElement("div") 
-gameTypeDiv.id = 'gameTypeDiv'
-gameInfoAreaDiv.append(gameTypeDiv);
-
-let gameTypeRadioForm = document.createElement("form")
-gameTypeRadioForm.id = 'gameTypeRadioForm'
-gameTypeDiv.append(gameTypeRadioForm);
-
-
-let twoPlayerChoiceDiv = document.createElement('div') 
-twoPlayerChoiceDiv.setAttribute('id', 'twoPlayerChoiceDiv')
-gameTypeRadioForm.append(twoPlayerChoiceDiv)
-
-let twoPlayerInput = document.createElement('input') 
-twoPlayerInput.setAttribute('type', 'radio')
-twoPlayerInput.setAttribute('id', '2P')
-twoPlayerInput.setAttribute('name', 'gameType')
-twoPlayerInput.setAttribute('value', '2 Player')
-twoPlayerChoiceDiv.append(twoPlayerInput)
-
-let twoPlayerLabel = document.createElement('label')
-twoPlayerLabel.setAttribute('for', '2P')
-twoPlayerLabel.innerHTML = '2P'
-twoPlayerChoiceDiv.append(twoPlayerLabel)
-
-
-let onePlayerEasyDiv = document.createElement('div') 
-gameTypeRadioForm.append(onePlayerEasyDiv)
-
-let onePlayerEasyInput = document.createElement('input') 
-onePlayerEasyInput.setAttribute('type', 'radio')
-onePlayerEasyInput.setAttribute('id', '1PE')
-onePlayerEasyInput.setAttribute('name', 'gameType')
-onePlayerEasyInput.setAttribute('value', '1 Player Easy')
-onePlayerEasyDiv.append(onePlayerEasyInput)
-
-let onePlayerEasyLabel = document.createElement('label')
-onePlayerEasyLabel.setAttribute('for', '1PE')
-onePlayerEasyLabel.innerHTML = '1P Easy'
-onePlayerEasyDiv.append(onePlayerEasyLabel)
-
-
-let onePlayerHardDiv = document.createElement('div') 
-gameTypeRadioForm.append(onePlayerHardDiv)
-
-let onePlayerHardInput = document.createElement('input') 
-onePlayerHardInput.setAttribute('type', 'radio')
-onePlayerHardInput.setAttribute('id', '1PH')
-onePlayerHardInput.setAttribute('name', 'gameType')
-onePlayerHardInput.setAttribute('value', '1 Player Hard')
-onePlayerHardDiv.append(onePlayerHardInput)
-
-let onePlayerHardLabel = document.createElement('label')
-onePlayerHardLabel.setAttribute('for', '1PE')
-onePlayerHardLabel.innerHTML = '1P Hard'
-onePlayerHardDiv.append(onePlayerHardLabel)
-
-
 function validateRadio() {
-
-    play2PGame()
-        // var ele = document.getElementsByName('gameType');    
-    //         for(i = 0; i < ele.length; i++) {
-    //             if(ele[i].checked)
-    //             alert(ele[i].value)
-    //         }
+    let ele = document.getElementsByName('gameType');    
+            for(i = 0; i < ele.length; i++) {
+                if(ele[i].checked){
+                gameType = (ele[i].value)
+                beginGame()
+                }
+            }
 };
 
+function beginGame() {
+    if (gameType == "1PEasy"){
+        itsAOnePlayerGame = true
+        resetBoard()
+        onePlayerPickSides()
+        play1PGame()
+        }
+    else if (gameType == "1PHard"){
+        itsAOnePlayerGame = true
+        resetBoard()
+        onePlayerPickSides()
+        play1PGame()
+        }
+    else if (gameType == "2P"){
+        itsAOnePlayerGame = false
+        resetBoard()
+        twoPlayerPickSides()
+        play2PGame()
+        }
+};
 
+function resetBoard() {
+    playerOneTurn = true
+    isThereAWinner = false
+    gameboard = [
+        [1,2,3,4,5,6,7],
+        [8,9,10,11,12,13,14],
+        [15,16,17,18,19,20,21],
+        [22,23,24,25,26,27,28],
+        [29,30,31,32,33,34,35],
+        [36,37,38,39,40,41,42]
+       ];
+}
 
-// Begin Game
+function onePlayerPickSides(){
+    playerOne = prompt('Your name')
+    playerTwo = 'Computer'
+};
 
-// function oneOrTwoPlayerGame(){
-//     typeOfGame = prompt('One or Two Player')
-//     if (typeOfGame == "1"){
-//         itsAOnePlayerGame = true
-//         onePlayerPickSides()
-//         onePlayerSimpleOrAdvanced()
-//         }
-//     else if (typeOfGame == "2"){
-//         itsAOnePlayerGame = false
-//         twoPlayerPickSides()
-//         play2PGame()
-//         }
-//     else {alert("No")
-//         isThereAWinner = true
-//         return
-//         }
-// };
-
-// function onePlayerPickSides(){
-//     playerOne = prompt('Your name')
-//     playerTwo = 'Computer'
-// };
-
-// function twoPlayerPickSides() {
-//     playerOne = prompt('Player One name')
-//     playerTwo = prompt('Player Two name')
-// };
-
-// function onePlayerSimpleOrAdvanced(){
-//     simpleOrAdvanced = prompt('Simple(1) or Advanced(2)')
-//     if (simpleOrAdvanced == 1) {
-//         play1PGame()
-//     }
-//     else if (simpleOrAdvanced == 2) {
-//         playAdvanced1PGame()
-//     }
-//     else {alert("No")
-//         isThereAWinner = true
-//         return
-//         }
-// };
+function twoPlayerPickSides() {
+    playerOne = prompt('Player One name')
+    playerTwo = prompt('Player Two name')
+};
 
 function whosPlaying() {
     if(playerOneTurn) {
@@ -217,20 +161,12 @@ function whosPlaying() {
     }
 };
 
-// function play1PGame() {
-//     while (isThereAWinner == false) {
-//         playerSelects1P()
-//         placeToken()
-//     }
-// };
-
-// function playAdvanced1PGame() {
-//     while (isThereAWinner == false) {
-//         playerSelects1P()
-//         // advancedPlayerSelects1P()
-//         placeToken()
-//     }
-// };
+function play1PGame() {
+    while (isThereAWinner == false) {
+        playerSelects1P()
+        placeToken()
+    }
+};
 
 function play2PGame() {
     while (isThereAWinner == false) {
@@ -239,16 +175,21 @@ function play2PGame() {
     }
 };
 
-
 function swapTurns() {
     playerOneTurn = !playerOneTurn
+    selectorTable.innerHTML = ""
+    drawSelector()
 };
 
 // GAMEPLAY
 
 function playerSelects2P() {
     findAvailableSpots()
+
+    // put an eventListener here?
     columnPick = prompt(whosPlaying() +  ', choose which column 1-7')
+
+
     if (availableSpots.includes(parseInt(columnPick))) 
         {console.log(columnPick)}
     else {
@@ -261,11 +202,7 @@ function findAvailableSpots() {
     availableSpots = gameboard[0].filter(x => Number.isInteger(x) == true)
 };
 
-
-
 function playerSelects1P() {
-    // alert("here is 1P (149)")
-    // alert(whosPlaying() + " (151) " + availableSpots)
     if (whosPlaying() == playerTwo) {
         findAvailableSpots()
         columnPick = availableSpots[Math.floor(Math.random() * availableSpots.length)]
@@ -275,35 +212,55 @@ function playerSelects1P() {
 };    
 
 function tokenClassesForGameboard() {
-    if(playerOneTurn) {
+    if (playerOneTurn) {
     return "One"
     } else {
     return "Two"
     }
 };
 
-// starts from the bottom row and places token in chosen spot when there it is a number (unoccupied)
-function placeToken() {
+// starts from the bottom row and claims spot when there it is a number (unoccupied)
+
+function claimSpot(){
     let i;
     for (i = 5; i > -1; i--) 
-        {if (Number.isInteger(gameboard[i][columnPick-1])) {
-            currentIndex = (columnPick -1)
-            currentRow = i
-            alert(whosPlaying() + " choice is " + columnPick)
+        {if (Number.isInteger(gameboard[i][indexPick])) {
+            gameboard[i].splice((indexPick), 1, tokenClassesForGameboard())
+            console.log(gameboard)
 
-// I need to splice in something else here.
-            gameboard[i].splice((columnPick -1), 1, tokenClassesForGameboard())
+            mainTable.innerHTML = ""
+            drawBoard()
 
             horizontalCheck()
             verticalCheck()
             downrightCheck()
             uprightCheck()
 
-            swapTurns()
             return
-        }
+            }
         }
 };
+
+
+// function placeToken() {
+//     let i;
+//     for (i = 5; i > -1; i--) 
+//         {if (Number.isInteger(gameboard[i][columnPick-1])) {
+//             currentIndex = (columnPick -1)
+//             currentRow = i
+//             alert(whosPlaying() + " choice is " + columnPick)
+
+//             gameboard[i].splice((columnPick -1), 1, tokenClassesForGameboard())
+
+//             horizontalCheck()
+//             verticalCheck()
+//             downrightCheck()
+//             uprightCheck()
+
+//             return
+//         }
+//         }
+// };
 
 // WIN CHECKERS
 // a forloop evaluates a section of the matrix, moving through it and seeing if the 3 ahead match.
@@ -317,7 +274,7 @@ function findFour(w,x,y,z) {
 
 function winDeclared() {
     isThereAWinner = true
-    console.log(whosPlaying() + " wins!")
+    alert(whosPlaying() + " wins!")
     console.log(gameboard)
 };
 
@@ -368,3 +325,69 @@ function horizontalCheck() {
     }
 };
 
+
+
+// REMOVE:
+// Draw gameTypeDropDownArea
+// to avoid the show function being written for a button that won't exist when the function is parsed I used radio buttons
+
+// let gameInfoAreaDiv = document.createElement("div") 
+// gameInfoAreaDiv.id = 'gameInfoArea'
+// mainDiv.append(gameInfoAreaDiv);
+
+// let gameTypeDiv = document.createElement("div") 
+// gameTypeDiv.id = 'gameTypeDiv'
+// gameInfoAreaDiv.append(gameTypeDiv);
+
+// let gameTypeRadioForm = document.createElement("form")
+// gameTypeRadioForm.id = 'gameTypeRadioForm'
+// gameTypeDiv.append(gameTypeRadioForm);
+
+
+// let twoPlayerChoiceDiv = document.createElement('div') 
+// twoPlayerChoiceDiv.setAttribute('id', 'twoPlayerChoiceDiv')
+// gameTypeRadioForm.append(twoPlayerChoiceDiv)
+
+// let twoPlayerInput = document.createElement('input') 
+// twoPlayerInput.setAttribute('type', 'radio')
+// twoPlayerInput.setAttribute('id', '2P')
+// twoPlayerInput.setAttribute('name', 'gameType')
+// twoPlayerInput.setAttribute('value', '2 Player')
+// twoPlayerChoiceDiv.append(twoPlayerInput)
+
+// let twoPlayerLabel = document.createElement('label')
+// twoPlayerLabel.setAttribute('for', '2P')
+// twoPlayerLabel.innerHTML = '2P'
+// twoPlayerChoiceDiv.append(twoPlayerLabel)
+
+
+// let onePlayerEasyDiv = document.createElement('div') 
+// gameTypeRadioForm.append(onePlayerEasyDiv)
+
+// let onePlayerEasyInput = document.createElement('input') 
+// onePlayerEasyInput.setAttribute('type', 'radio')
+// onePlayerEasyInput.setAttribute('id', '1PE')
+// onePlayerEasyInput.setAttribute('name', 'gameType')
+// onePlayerEasyInput.setAttribute('value', '1 Player Easy')
+// onePlayerEasyDiv.append(onePlayerEasyInput)
+
+// let onePlayerEasyLabel = document.createElement('label')
+// onePlayerEasyLabel.setAttribute('for', '1PE')
+// onePlayerEasyLabel.innerHTML = '1P Easy'
+// onePlayerEasyDiv.append(onePlayerEasyLabel)
+
+
+// let onePlayerHardDiv = document.createElement('div') 
+// gameTypeRadioForm.append(onePlayerHardDiv)
+
+// let onePlayerHardInput = document.createElement('input') 
+// onePlayerHardInput.setAttribute('type', 'radio')
+// onePlayerHardInput.setAttribute('id', '1PH')
+// onePlayerHardInput.setAttribute('name', 'gameType')
+// onePlayerHardInput.setAttribute('value', '1 Player Hard')
+// onePlayerHardDiv.append(onePlayerHardInput)
+
+// let onePlayerHardLabel = document.createElement('label')
+// onePlayerHardLabel.setAttribute('for', '1PE')
+// onePlayerHardLabel.innerHTML = '1P Hard'
+// onePlayerHardDiv.append(onePlayerHardLabel)
